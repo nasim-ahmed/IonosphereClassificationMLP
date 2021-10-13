3bd37198604583e19b50f0e28df75dde9e084c38
nasim-ahmed <nasim.ahmed54@gmail.com>
2021-10-13 18:00:52 +0200

    model added

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from os import initgroups
 from numpy import vstack
 from pandas import read_csv
 from sklearn.preprocessing import LabelEncoder
@@ -23,7 +24,7 @@ class CSVDataset(Dataset):
         self.Y = df.values[:, -1]
         self.X = self.X.astype('float32')
         #label encode the target variable
-        self.Y = LabelEncoder.fit_transform(self.Y)
+        self.Y = LabelEncoder().fit_transform(self.Y)
         self.Y = self.Y.astype('float32')
         self.Y = self.Y.reshape(len(self.Y), 1)
 
@@ -41,6 +42,31 @@ class CSVDataset(Dataset):
         #calculate the split
         return random_split(self, [train_size, test_size])
 
+#define model
+class MLP(Module):
+    def __init__(self, n_inputs):
+        super(MLP, self).__init__()
+        self.hidden1 = Linear(n_inputs,10)
+        kaiming_uniform_(self.hidden1.weights, nonlinearity='relu')
+        self.act1 = ReLU()
+        self.hidden2 = Linear(10,8)
+        kaiming_uniform_(self.hidden2.weights, nonlinearity='relu')
+        self.act2 = ReLU()
+        self.hidden3 = Linear(8,1)
+        xavier_uniform_(self.hidden3.weights)
+        self.act3 = Sigmoid()
+
+    def forward(self, X):
+        X = self.hidden1(X)
+        X = self.act1(X)
+        X = self.hidden2(X)
+        X = self.act2(X)
+        X = self.hidden3(X)
+        X = self.act3(X)
+        return X
+
+
+
 
 #prepare the dataset
 def prepare_data(path):
@@ -52,7 +78,9 @@ def prepare_data(path):
 
 
 
-
+train_dl, test_dl = prepare_data('ionosphere.csv')
+print(type(train_dl))
+print('The length of the train data: {0}, test data:{1}'.format(len(train_dl.dataset), len(test_dl.dataset)))
 
 
 
